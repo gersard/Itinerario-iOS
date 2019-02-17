7f7a3c42970bfa06c6d1e9796d56f3d45ea0d038
Gerardo Mascayano <gerardo.mascayano@gmail.com>
2019-02-16 23:34:12 -0300

    - Creado storyboard y view controller para agregar trip

---
+++ Itinerary/Views/AddTripViewController/AddTripVC.swift
@@ -0,0 +1,30 @@
+//
+//  AddTripVC.swift
+//  Itinerary
+//
+//  Created by Gerardo Mascayano on 2/16/19.
+//  Copyright © 2019 Gerardo Mascayano. All rights reserved.
+//
+
+import UIKit
+
+class AddTripVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
